d640d29f3ae11a4074a0fe14ea9456aabd9341b0
Tan477 <tan4778@gmail.com>
2014-09-28 00:43:37 +0700

    Initial commit

---
+++ TestApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestApp
+//
+//  Created by Tan Ophaswongse on 9/28/14.
+//  Copyright (c) 2014 Tan Ophaswongse. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestApp
+//
+//  Created by Tan Ophaswongse on 9/28/14.
+//  Copyright (c) 2014 Tan Ophaswongse. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
